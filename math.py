70613d2e02fa4a72b6bb03bdb4f7334f1273a954
Santosh K Tandale <santoshtandale@gmail.com>
2016-02-06 14:47:50 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+print "Now time for some maths"
+
+print "first" , 2 + 5 - 9
+print "second" , 100 - 25 * 3 % 4
+print "third" , 12 * 3 - 1
+print "fourth" , 3 + 4 * 7
+print "fifth" , 7 + 9 % 2
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 < 5 - 7
+print "sixth" , 13 / 4
+print "seventh" , 13.0 / 2.0
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+
+print "7 / 4 is ", 7 / 4
+print "7.0 / 4.0 is ", 7.0 / 4.0
+
+ 
+
